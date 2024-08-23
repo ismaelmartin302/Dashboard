82c2e4f194e18b9f8189635949a529c38ca40a76
isma <ismaelmartin302@gmail.com>
2024-08-24 01:29:26 +0200

    Que complicado son los grids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SchemaComponent } from "./modules/schema/schema.component";
+import { MenuComponent } from "./layout/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SchemaComponent],
+  imports: [RouterOutlet, SchemaComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
