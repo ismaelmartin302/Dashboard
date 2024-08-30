2f365f84d1ec2f7d4be5530f6a9b34d7b6ed3944
isma <ismaelmartin302@gmail.com>
2024-08-31 00:44:41 +0200

    "Updated menu component to display dynamic time and added browser platform check"

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  public actualTime: Date = new Date();
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      setInterval(() => {
+        this.actualTime = new Date();
+      }, 60000);
+    }
+  }
 }
